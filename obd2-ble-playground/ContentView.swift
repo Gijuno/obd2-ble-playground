b9f4903c141e1a20aa1cb117b2695cc6a435b88c
Gijuno <46197097+Gijuno@users.noreply.github.com>
2024-01-19 21:34:16 +0900

    Initial Commit

---
+++ obd2-ble-playground/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  obd2-ble-playground
+//
+//  Created by gijuno on 1/19/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ obd2-ble-playground/obd2_ble_playgroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  obd2_ble_playgroundApp.swift
+//  obd2-ble-playground
+//
+//  Created by gijuno on 1/19/24.
+//
+
+import SwiftUI
+
+@main
+struct obd2_ble_playgroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
